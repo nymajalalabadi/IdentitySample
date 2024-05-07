ff90acec550cfb5ec310bd56a294537c2deee887
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-07 21:05:31 +0330

    Added Roler For User

---
+++ IdentitySample/ViewModels/ManageUser/AddUserToRoleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace IdentitySample.ViewModels.ManageUser
+{
+    public class AddUserToRoleViewModel
+    {
+        public AddUserToRoleViewModel()
+        {
+            UserRoles = new List<UserRolesViewModel>();
+        }
+
+        public string UserId { get; set; }
+
+        public List<UserRolesViewModel> UserRoles { get; set; }
+    }
+
+
+    public class UserRolesViewModel
+    {
+        public string RoleName { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
---
+++ IdentitySample/ViewModels/ManageUser/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentitySample.ViewModels.ManageUser
+{
+    public class IndexViewModel
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+}
