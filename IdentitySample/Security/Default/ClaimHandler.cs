5ef9e4a3af67fca9d728bf500479bea3130f71bf
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-11 23:50:39 +0330

    Added Claim Handler

---
+++ IdentitySample/Security/Default/ClaimHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+
+namespace IdentitySample.Security.Default
+{
+    public class ClaimHandler : AuthorizationHandler<ClaimRequirement>
+    {
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ClaimRequirement requirement)
+        {
+            if (context.User.HasClaim(requirement.ClaimType, requirement.ClaimValue))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ IdentitySample/Security/Default/ClaimRequirement.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace IdentitySample.Security.Default
+{
+    public class ClaimRequirement : IAuthorizationRequirement
+    {
+        public ClaimRequirement(string claimType, string claimValue)
+        {
+            ClaimType = claimType;
+            ClaimValue = claimValue;
+        }
+
+        public string ClaimType { get; }
+        public string ClaimValue { get; }
+    }
+
+}
