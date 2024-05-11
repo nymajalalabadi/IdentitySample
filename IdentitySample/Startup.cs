25a1417b35d80db9d47e8c97b59f914622a03df7
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-12 00:12:19 +0330

    Added Services AddSingleton for ClaimHandler

--- IdentitySample/Startup.cs
+++ IdentitySample/Startup.cs
@@ -86,6 +86,7 @@ namespace IdentitySample
             #endregion
 
             services.AddScoped<IMessageSender, MessageSender>();
+            services.AddSingleton<IAuthorizationHandler, ClaimHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
