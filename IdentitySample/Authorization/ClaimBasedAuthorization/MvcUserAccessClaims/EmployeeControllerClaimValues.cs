f25ee47d2034850ef1eef9f97806edb1764a6cc4
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-05-23 20:55:56 +0330

    Added EmployeeControllerClaimValues

---
+++ IdentitySample/Authorization/ClaimBasedAuthorization/MvcUserAccessClaims/EmployeeControllerClaimValues.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace IdentitySample.Authorization.ClaimBasedAuthorization.MvcUserAccessClaims
+{
+    public static class EmployeeControllerClaimValues
+    {
+        public const string EmployeeIndex = nameof(EmployeeIndex);
+        public const string EmployeeIndexPersian = "صفحه اصلی مدیریت کارکنان";
+
+        public const string EmployeeDetails = nameof(EmployeeDetails);
+        public const string EmployeeDetailsPersian = "جزئیات کارکنان";
+
+        public static ReadOnlyCollection<(string claimValueEnglish, string claimValuePersian)> AllClaimValues;
+
+        static EmployeeControllerClaimValues()
+        {
+            AllClaimValues = MvcClaimValuesUtilities.GetPersianAndEnglishClaimValues(typeof(EmployeeControllerClaimValues))
+                    .ToList()
+                    .AsReadOnly();
+        }
+    }
+
+}
