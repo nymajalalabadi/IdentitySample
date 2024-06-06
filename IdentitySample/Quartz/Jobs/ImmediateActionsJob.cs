c1ba6ff653280b6d3664b2e0788b43e0f1eb080c
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-07 00:01:43 +0330

    Added Quartz

---
+++ IdentitySample/Quartz/Jobs/ImmediateActionsJob.cs
@@ -0,0 +1,38 @@
+﻿using IdentitySample.Models.Context;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using System.Linq;
+
+namespace IdentitySample.Quartz.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class ImmediateActionsJob : IJob
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ImmediateActionsJob(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            if (await dbContext.ImmediateActions.AnyAsync())
+            {
+                var expiredActions = await dbContext.ImmediateActions
+                    .Where(i => i.ExpirationTime < DateTime.Now)
+                    .ToListAsync();
+
+                dbContext.RemoveRange(expiredActions);
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+
+}
