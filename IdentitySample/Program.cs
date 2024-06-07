c59c6b3841c2bbd3248984bc177a927e95bb7371
nymajalalabadi <nima.jalalabadii@gmail.com>
2024-06-08 00:21:36 +0330

    Added SyncImmediateActionsStores

--- IdentitySample/Program.cs
+++ IdentitySample/Program.cs
@@ -1,5 +1,9 @@
+using IdentitySample.Models.Context;
+using IdentitySample.Services;
+using Kaktos.UserImmediateActions.Stores;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,10 +15,22 @@ namespace IdentitySample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var mainStore = scope.ServiceProvider.GetRequiredService<IImmediateActionsStore>();
+
+                var syncImmediateActionsStores = new SyncImmediateActionsStores(dbContext, mainStore);
+                await syncImmediateActionsStores.SyncImmediateActionsFromPermanentStoreToMainStoreAsync();
+            }
+
+            await host.RunAsync();
         }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
---
+++ IdentitySample/Services/SyncImmediateActionsStores.cs
@@ -0,0 +1,44 @@
+﻿using IdentitySample.Models.Context;
+using Kaktos.UserImmediateActions.Models;
+using Kaktos.UserImmediateActions.Stores;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace IdentitySample.Services
+{
+    public class SyncImmediateActionsStores
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IImmediateActionsStore _mainStore;
+
+        public SyncImmediateActionsStores(AppDbContext dbContext, IImmediateActionsStore mainStore)
+        {
+            _dbContext = dbContext;
+            _mainStore = mainStore;
+        }
+
+        public async Task SyncImmediateActionsFromPermanentStoreToMainStoreAsync()
+        {
+            if (!await _dbContext.ImmediateActions.AnyAsync())
+            {
+                return;
+            }
+
+            var actions = await _dbContext.ImmediateActions
+                .Where(i => i.ExpirationTime > DateTime.Now)
+                .ToListAsync();
+
+            foreach (var action in actions)
+            {
+                var expirationTime = action.ExpirationTime - DateTime.Now;
+
+                await _mainStore.AddAsync(action.ActionKey, 
+                    expirationTime,
+                    new ImmediateActionDataModel(action.AddedDate, action.Purpose),
+                    false);
+            }
+        }
+    }
+}
